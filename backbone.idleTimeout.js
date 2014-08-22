53eb8eca1fb0aa2b4b64b0721682a922e5f78c55
Steve Willard <stevewillard@gmail.com>
2014-08-22 00:31:00 -0400

    Bug fixing - using localStorage instead of sessionStorage

--- backbone.idleTimeout.js
+++ backbone.idleTimeout.js
@@ -5,69 +5,72 @@
 }(function(Backbone, _) {
     var LOCAL_STORAGE_KEY = "idleTimeout.actionTime";
 
-    function IdleTimeout() {
+    function IdleTimeout(options) {
         _.extend(this, Backbone.Events);
         this.timeout = null;
+        this.lastAction = null;
         this.warningFired = false;
-    }
 
-    IdleTimeout.prototype.init = function(options) {
-        this.options = _.defaults(options, {
+        this.options = _.defaults(options || {}, {
             "checkIntervalSecs" : 10,
             "events" : {
                 "document" : "click keyup"
             },
-            "warningDurationSecs" : 60 * 14,
-            "triggerDurationSecs" : 60 * 15
+            "warningSecs" : 60 * 14,
+            "timeoutSecs" : 60 * 15
         });
-
-        console.log(this.options);
 
         // Apply action callbacks
-        _.each(_.keys(this.options.events), function(selector) {
+        _.each(_.keys(this.options.events), _.bind(function(selector) {
             var s = (selector === "document" ? document : selector);
             Backbone.$(s).on(this.options.events[selector], _.bind(this.actionCallback, this));
-        });
-    };
+        }, this));
+    }
 
     IdleTimeout.prototype.actionCallback = function() {
-        console.log("action callback");
-        sessionStorage.setItem(LOCAL_STORAGE_KEY, (new Date()).getTime());
+        localStorage.setItem(LOCAL_STORAGE_KEY, (new Date()).getTime());
+        this.trigger("action");
     };
 
     IdleTimeout.prototype.checkCallback = function() {
         console.log("check callback");
-        var lastActionUnixTime = parseInt(sessionStorage.getItem(LOCAL_STORAGE_KEY), 10),
+        var lastActionUnixTime = parseInt(localStorage.getItem(LOCAL_STORAGE_KEY), 10),
             timeDiff = ((new Date()).getTime() - (new Date(lastActionUnixTime)).getTime()) / 1000;
 
-        if (!this.warningFired && timeDiff >= this.options.warningDurationSecs) {
+        // Store the last time an action happened
+        if (this.lastAction && this.lastAction !== lastActionUnixTime) {
+            this.trigger("action");
+            this.warningFired = false;
+            this.lastAction = lastActionUnixTime;
+            return;
+        }
+        this.lastAction = lastActionUnixTime;
+
+        // Fire warning
+        if (!this.warningFired && timeDiff >= this.options.warningSecs) {
             this.trigger("warning");
             this.warningFired = true;
         }
 
-        if (timeDiff >= this.options.triggerDurationSecs) {
+        // Fire timeout
+        if (timeDiff >= this.options.timeoutSecs) {
             this.trigger("timeout");
         }
     };
 
     IdleTimeout.prototype.start = function() {
-        console.log("started");
         this.stop();
         this.actionCallback();
-        console.log("options: ");
-        console.log(this.options);
         this.timeout = setInterval(_.bind(this.checkCallback, this),
                 this.options.checkIntervalSecs * 1000);
     };
 
     IdleTimeout.prototype.stop = function() {
-        console.log("stopped");
         if (this.timeout) {
             clearTimeout(this.timeout);
             this.timeout = null;
         }
         this.warningFired = false;
-        sessionStorage.removeItem(LOCAL_STORAGE_KEY);
     };
 
     return IdleTimeout;
